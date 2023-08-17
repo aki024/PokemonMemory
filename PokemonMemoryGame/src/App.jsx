b3ea847459cc278cf1ba53e28e9718f91b70717d
aki024 <aleksa.simicc7@gmail.com>
2023-08-17 14:23:46 +0200

    adding boilerplate files just to get the logic correct

--- PokemonMemoryGame/src/App.jsx
+++ PokemonMemoryGame/src/App.jsx
@@ -1,9 +1,20 @@
 import './styles/App.scss';
 import Main from './components/Main/Main';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 	const [gameRunning, setGameRunning] = useState(false);
+	useEffect(() => {
+		const getPokemon = async () => {
+			const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100000&offset=0`);
+			const test = await res.json();
+			const { results } = test;
+			console.log(results);
+			return results;
+		};
+		let a = getPokemon();
+		console.log(a);
+	}, []);
 
 	return (
 		<>
--- PokemonMemoryGame/src/components/Main/Main.jsx
+++ PokemonMemoryGame/src/components/Main/Main.jsx
@@ -1,55 +1,21 @@
 import { useState } from 'react';
 import styles from './Main.module.scss';
 import menuImage from '../../assets/images/menu.jpg';
-import easy from '../../assets/images/Easy.jpg';
-import normal from '../../assets/images/Normal.jpg';
-import hard from '../../assets/images/Hard.png';
+
+import Modal from '../Modal/Modal';
+import OptionMenu from '../OptionMenu/OptionMenu';
+import Game from '../Game/Game';
 
 const Main = ({ gameRunning, setGameRunning }) => {
-	const cardGoals = [5, 10, 18];
 	const [difficulty, setDifficulty] = useState(menuImage);
-
-	const [cardGoal, setCardGoal] = useState(cardGoals[0]);
-
-	const handleStartGame = (e) => {
-		e.preventDefault();
-		switch (cardGoal) {
-			case '10':
-				setDifficulty(normal);
-				setGameRunning(true);
-				break;
-			case '18':
-				setDifficulty(hard);
-				setGameRunning(true);
-				break;
-			default:
-				setDifficulty(easy);
-				setGameRunning(true);
-		}
-	};
-
-	const handleChange = (newCardGoal) => {
-		setCardGoal(newCardGoal);
-
-		// playAudio(clickAudio);
-	};
-
 	return (
 		<main className={styles.main} style={{ backgroundImage: `url(${difficulty})` }}>
-			<form onSubmit={handleStartGame}>
-				<div className='level-options'>
-					<label htmlFor='easy'>Easy</label>
-					<input type='radio' id='easy' name='test' value={cardGoals[0]} defaultChecked onChange={(e) => handleChange(e.target.value)} />
-					<label htmlFor='normal'>Normal</label>
-					<input type='radio' id='normal' name='test' value={cardGoals[1]} onChange={(e) => handleChange(e.target.value)} />
-					<label htmlFor='hard'>Hard</label>
-					<input type='radio' id='hard' name='test' value={cardGoals[2]} onChange={(e) => handleChange(e.target.value)} />
-				</div>
-
-				<div className='start-options'>
-					<button type='submit'>Click me</button>
-				</div>
-			</form>
+			{!gameRunning && (
+				<Modal>
+					<OptionMenu gameRunning={gameRunning} setGameRunning={setGameRunning} setDifficulty={setDifficulty} />
+				</Modal>
+			)}
+			{gameRunning && <Game />}
 		</main>
 	);
 };
